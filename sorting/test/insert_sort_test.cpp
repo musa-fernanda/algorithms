405b98909bb94db3505e585b643608d17c542701
Musa Fernanda <musafernanda12@proton.me>
2023-05-11 00:48:02 +0800

    test: add testing for `insertion sort` algorithm

---
+++ sorting/test/insert_sort_test.cpp
@@ -0,0 +1,19 @@
+#include "../insert_sort.h"
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <vector>
+
+TEST(Sorting, InsertSortTest) {
+  // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
+  auto vec = std::vector<int>{};
+  auto expected = std::vector<int>{};
+
+  vec = std::vector<int>{3, 2, 1, 5, 4};
+  expected = std::vector<int>{1, 2, 3, 4, 5};
+  mf::sorting::insert_sort(vec);
+
+  ASSERT_TRUE(std::is_sorted(vec.begin(), vec.end()));
+  ASSERT_TRUE(std::is_sorted(expected.begin(), expected.end()));
+  ASSERT_EQ(vec, expected);
+  // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
+};
